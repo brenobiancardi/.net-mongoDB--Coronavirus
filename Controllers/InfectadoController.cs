f9116bec757e3c7e323a2b528131204c6cea02a4
breno.sousa <breno.sousa@sefaz.es.gov.br>
2021-01-07 11:02:52 -0300

    first commit

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,39 @@
+﻿using projetoAvanade.Data.Collections;
+using projetoAvanade.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace projetoAvanade.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        readonly Data.MongoDB _mongoDB;
+        readonly IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+            
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            
+            return Ok(infectados);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Controle de infectados por coronavirus
+
+Tecnologias utilizadas:
+
+<img width="35px" src="./Csharp.svg">
+<img width="35px" height="35px" src="./netcore.svg">
+<img width="70px" src="./mongo.svg">
+
+Projeto desenvolvido durante o Bootcamp Avanade Fullstack developer que participei com o objetivo de aprimorar meus conhecimentos em Angular, .NET CORE e TDD.
+
+Essa aplicação consiste em uma API para CRUD de infectados por Coronavirus.
